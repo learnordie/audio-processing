86b8c3a815ad1f7c7ba3fa020c2562c88c67393e
learnordie <learnordie@yandex.com>
2018-07-25 21:44:30 +0200

    Add class "Audio"

---
+++ audio.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Declaration of the class Audio """
+
+
+from scipy.io import wavfile
+
+
+class Audio:
+    def __init__(self, filename):
+        """ Initialize the class only reading a WAV file.
+
+            Args:
+                filename: WAV audio file.
+
+        """
+        # scipy handles exceptions if a problem with the file happens
+        rate, data = wavfile.read(filename)
+        self.rate = rate
+        self.data = data
+
+    def sampling_rate(self):
+        """ Return the sampling rate (in samples/sec, or Hz). """
+        return self.rate
+
+    def is_mono(self):
+        """ Check if a WAV audio file is mono. """
+        # data.shape returns a tuple like (number_of_points,)
+        # if there is only one channel
+        return len(self.data.shape) == 1
+
+    def is_stereo(self):
+        """ Check if the audio is stereo or not. """
+        # data.shape returns a tuple: (number_of_points, number_of_channels)
+        # if the audio data is stereo (has two channels).
+        return len(self.data.shape) == 2
+
+    def number_of_sampling_points(self):
+        """ Return the number of sampling points. """
+        return self.data.shape[0]
+
+    def data_type(self):
+        """ Return the data type of an audio. """
+        return self.data.dtype
+
+    def duration(self):
+        """ Return the duration (in seconds) of an audio. """
+        npoints = self.number_of_sampling_points()
+        return npoints / self.rate
+
+    # def left_channel(self):
+    #     """ Return the left channel of an audio. """
+    #     return self.data[:, 0]
+
+    # def right_channel(self):
+    #     """ Return the right channel of an audio."""
+    #     if self.is_stereo():
+    #         return self.data[:, 1]
+    #     else:
+    #         return self.data[:, 0]
+
+    def max(self):
+        """ Return the maximum PCM (amplitude) value of the data. """
+        return self.data.max()
+
+    def min(self):
+        """ Return the minimum PCM (amplitude) value of the data. """
+        return self.data.min()
+
+
+# import os
+
+# filename4 = os.path.join(os.path.expanduser("~"),
+#                          "Projects", "audio_processing", "samples",
+#                          "harvard.wav")
+# harvard = Audio(filename4)
+# harvard.data[:,0]
---
+++ tests/test_audio.py
@@ -0,0 +1,215 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Testing read_audio.py """
+
+
+import sys
+import os
+import numpy
+import pytest
+
+# before importing modules, we need to append this path to the PYTHONPATH
+sys.path.append('$HOME/Projects/audio_processing')
+
+import audio
+
+
+# -------------------------------- #
+# Objects needed to do the testing #
+# -------------------------------- #
+
+filename1 = os.path.join(os.path.expanduser("~"),
+                         "Projects", "audio_processing", "samples",
+                         "stereo_44100_16bits.wav")
+stereo_44100_16bits = audio.Audio(filename1)
+
+filename2 = os.path.join(os.path.expanduser("~"),
+                         "Projects", "audio_processing", "samples",
+                         "mono_22050_16bits.wav")
+
+mono_22050_16bits = audio.Audio(filename2)
+
+filename3 = os.path.join(os.path.expanduser("~"),
+                         "Projects", "audio_processing", "samples",
+                         "mono_44100_32bits.wav")
+mono_44100_32bits = audio.Audio(filename3)
+
+filename4 = os.path.join(os.path.expanduser("~"),
+                         "Projects", "audio_processing", "samples",
+                         "harvard.wav")
+harvard = audio.Audio(filename4)
+
+# --------------------- #
+# Functions for testing #
+# --------------------- #
+
+
+def test_read_wavfile_1():
+    """ Test the function read_wavfile
+        when its argument "filename" doesn't exist.
+    """
+
+    filename = os.path.join(os.path.expanduser("~"), "empty.file")
+    with pytest.raises(Exception):
+        empty_audio = audio.Audio(filename)
+        print(empty_audio)
+
+
+def test_read_wavfile_2():
+    """ Test the function read_wavfile
+        with the file "mono_44100_32bits.wav".
+
+    I just check that not exception is raised.
+    """
+
+    filename = os.path.join(os.path.expanduser("~"),
+                            "Projects", "audio_processing", "samples",
+                            "stereo_44100_16bits.wav")
+
+    stereo_44100_16bits = audio.Audio(filename)
+    print(stereo_44100_16bits)
+
+
+def test_sampling_rate_1():
+    """ Test the sampling rate returned by the function read_wavfile
+        using the file 'stereo_44100_16bits.wav'.
+
+    """
+    assert stereo_44100_16bits.rate == 44100
+
+
+def test_sampling_rate_2():
+    """ Test the sampling rate returned by the function read_wavfile
+        using the file 'mono_22050_16bits.wav'.
+
+    """
+    assert mono_22050_16bits.rate == 22050
+
+
+def test_is_mono_1():
+    """ Test if an audio file is mono or not using the file
+        mono_22050_16bits.wav', which actually is mono.
+
+    """
+    assert mono_22050_16bits.is_mono()
+
+
+def test_is_mono_2():
+    """ Test if an audio file is mono or not using the file
+        'stereo_44100_16bits.wav', which actually is stereo.
+
+    """
+    assert not stereo_44100_16bits.is_mono()
+
+
+def test_is_stereo_1():
+    """ Test if an audio file is stereo or not using the file
+        'stereo_44100_16bits.wav', which actually is stereo.
+
+    """
+    assert stereo_44100_16bits.is_stereo()
+
+
+def test_is_stereo_2():
+    """ Test if an audio file is stereo or not using the file
+        mono_22050_16bits.wav', which actually is mono.
+
+    """
+    assert not mono_22050_16bits.is_stereo()
+
+
+def test_number_of_sampling_points_1():
+    """ Test the number of sampling points using the file
+        'stereo_44100_16bits.wav', which actually is stereo.
+
+    """
+    assert stereo_44100_16bits.number_of_sampling_points() == 294909
+
+
+def test_number_of_sampling_points_2():
+    """ Test the number of sampling points using the file
+        mono_22050_16bits.wav', which actually is mono.
+
+    """
+    assert mono_22050_16bits.number_of_sampling_points() == 147455
+
+
+def test_data_type_1():
+    """ Test the data type of the file 'mono_44100_32bits',
+        which actually is int32.
+
+    """
+    assert mono_44100_32bits.data_type() == "int32"
+
+
+def test_data_type_2():
+    """ Test the data type of the file 'mono_22050_16bits.wav',
+        which actually is int16.
+
+    """
+    assert mono_22050_16bits.data_type() == "int16"
+
+
+def test_duration_1():
+    """ Test the duration (in seconds) of the file
+    'mono_22050_16bits.wav', which actually is 6.6873015873015875.
+
+    """
+    precision = 1e-15
+    assert (mono_22050_16bits.duration() - 6.6873015873015875) < precision
+
+
+def test_duration_2():
+    """ Test the duration (in seconds) of the file
+    'harvard.wav', which actually is 18.356190476190477.
+
+    """
+    precision = 1e-15
+    assert (harvard.duration() - 18.356190476190477) < precision
+
+
+# def test_left_channel_1():
+#     """ Test the left_channel method of a mono file 'mono_22050_16bits.wav',
+#     where data[:, 0] is the only channel.
+
+#     """
+#     # THIS MIGHT BE INCORRECT:
+#     # A channel is a tuple with two elements:
+#     #     - numpy array with the data of the audio
+#     #     - data type
+#     array1, dtype1 = mono_22050_16bits.left_channel()
+#     array2, dtype2 = mono_22050_16bits.data[:, 0]
+#     assert numpy.array_equal(array1, array2) and (dtype1 == dtype2)
+
+
+def test_max_1():
+    """ Test the maximum PCM (amplitude) value of the file
+    'mono_22050_16bits.wav', which actually is 32767.
+
+    """
+    assert mono_22050_16bits.max() == 32767
+
+
+def test_max_2():
+    """ Test the maximum PCM (amplitude) value of the file
+    'harvard.wav', which actually is 20329.
+
+    """
+    assert harvard.max() == 20329
+
+
+def test_min_1():
+    """ Test the minimum PCM (amplitude) value of the file
+    'mono_22050_16bits.wav', which actually is -32768.
+
+    """
+    assert mono_22050_16bits.min() == -32768
+
+
+def test_min_2():
+    """ Test the minimum PCM (amplitude) value of the file
+    'harvard.wav', which actually is -32768.
+
+    """
+    assert harvard.min() == -32768
