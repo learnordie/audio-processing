39aa24c6725a2eb8a9666274c70b0b4cae233654
learnordie <learnordie@yandex.com>
2018-08-01 12:21:24 +0000

    Change "absolute paths" to "relative paths" in the file "test_audio"

--- tests/test_audio.py
+++ tests/test_audio.py
@@ -4,40 +4,31 @@
 """ Testing read_audio.py """
 
 
-import sys
 import os
 import numpy
 import pytest
 
 # before importing modules, we need to append this path to the PYTHONPATH
-sys.path.append('$HOME/Projects/audio_processing')
+# sys.path.append('$HOME/Projects/audio_processing')
 
 import audio
 
+SAMPLES_DIR = os.path.join(os.path.dirname(""), "samples")
 
 # -------------------------------- #
 # Objects needed to do the testing #
 # -------------------------------- #
 
-filename1 = os.path.join(os.path.expanduser("~"),
-                         "Projects", "audio_processing", "samples",
-                         "stereo_44100_16bits.wav")
+filename1 = os.path.join(SAMPLES_DIR, "stereo_44100_16bits.wav")
 stereo_44100_16bits = audio.Audio(filename1)
 
-filename2 = os.path.join(os.path.expanduser("~"),
-                         "Projects", "audio_processing", "samples",
-                         "mono_22050_16bits.wav")
-
+filename2 = os.path.join(SAMPLES_DIR, "mono_22050_16bits.wav")
 mono_22050_16bits = audio.Audio(filename2)
 
-filename3 = os.path.join(os.path.expanduser("~"),
-                         "Projects", "audio_processing", "samples",
-                         "mono_44100_32bits.wav")
+filename3 = os.path.join(SAMPLES_DIR, "mono_44100_32bits.wav")
 mono_44100_32bits = audio.Audio(filename3)
 
-filename4 = os.path.join(os.path.expanduser("~"),
-                         "Projects", "audio_processing", "samples",
-                         "harvard.wav")
+filename4 = os.path.join(SAMPLES_DIR, "harvard.wav")
 harvard = audio.Audio(filename4)
 
 # --------------------- #
@@ -47,7 +38,7 @@ harvard = audio.Audio(filename4)
 
 def test_read_wavfile_1():
     """ Test the function read_wavfile
-        when its argument "filename" doesn't exist.
+        when its argument 'filename' doesn't exist.
     """
 
     filename = os.path.join(os.path.expanduser("~"), "empty.file")
@@ -73,7 +64,7 @@ def test_read_wavfile_2():
 
 def test_sampling_rate_1():
     """ Test the sampling rate returned by the function read_wavfile
-        using the file 'stereo_44100_16bits.wav'.
+        using the file "stereo_44100_16bits.wav".
 
     """
     assert stereo_44100_16bits.rate == 44100
@@ -81,7 +72,7 @@ def test_sampling_rate_1():
 
 def test_sampling_rate_2():
     """ Test the sampling rate returned by the function read_wavfile
-        using the file 'mono_22050_16bits.wav'.
+        using the file "mono_22050_16bits.wav".
 
     """
     assert mono_22050_16bits.rate == 22050
@@ -89,7 +80,7 @@ def test_sampling_rate_2():
 
 def test_is_mono_1():
     """ Test if an audio file is mono or not using the file
-        mono_22050_16bits.wav', which actually is mono.
+        "mono_22050_16bits.wav", which actually is mono.
 
     """
     assert mono_22050_16bits.is_mono()
@@ -97,7 +88,7 @@ def test_is_mono_1():
 
 def test_is_mono_2():
     """ Test if an audio file is mono or not using the file
-        'stereo_44100_16bits.wav', which actually is stereo.
+        "stereo_44100_16bits.wav", which actually is stereo.
 
     """
     assert not stereo_44100_16bits.is_mono()
@@ -105,7 +96,7 @@ def test_is_mono_2():
 
 def test_is_stereo_1():
     """ Test if an audio file is stereo or not using the file
-        'stereo_44100_16bits.wav', which actually is stereo.
+        "stereo_44100_16bits.wav", which actually is stereo.
 
     """
     assert stereo_44100_16bits.is_stereo()
@@ -113,7 +104,7 @@ def test_is_stereo_1():
 
 def test_is_stereo_2():
     """ Test if an audio file is stereo or not using the file
-        mono_22050_16bits.wav', which actually is mono.
+        "mono_22050_16bits.wav", which actually is mono.
 
     """
     assert not mono_22050_16bits.is_stereo()
@@ -121,7 +112,7 @@ def test_is_stereo_2():
 
 def test_number_of_sampling_points_1():
     """ Test the number of sampling points using the file
-        'stereo_44100_16bits.wav', which actually is stereo.
+        "stereo_44100_16bits.wav", which actually is stereo.
 
     """
     assert stereo_44100_16bits.number_of_sampling_points() == 294909
@@ -129,14 +120,14 @@ def test_number_of_sampling_points_1():
 
 def test_number_of_sampling_points_2():
     """ Test the number of sampling points using the file
-        mono_22050_16bits.wav', which actually is mono.
+        "mono_22050_16bits.wav", which actually is mono.
 
     """
     assert mono_22050_16bits.number_of_sampling_points() == 147455
 
 
 def test_data_type_1():
-    """ Test the data type of the file 'mono_44100_32bits',
+    """ Test the data type of the file "mono_44100_32bits.wav",
         which actually is int32.
 
     """
@@ -144,7 +135,7 @@ def test_data_type_1():
 
 
 def test_data_type_2():
-    """ Test the data type of the file 'mono_22050_16bits.wav',
+    """ Test the data type of the file "mono_22050_16bits.wav",
         which actually is int16.
 
     """
@@ -153,7 +144,7 @@ def test_data_type_2():
 
 def test_duration_1():
     """ Test the duration (in seconds) of the file
-    'mono_22050_16bits.wav', which actually is 6.6873015873015875.
+    "mono_22050_16bits.wav", which actually is 6.6873015873015875.
 
     """
     precision = 1e-15
@@ -162,7 +153,7 @@ def test_duration_1():
 
 def test_duration_2():
     """ Test the duration (in seconds) of the file
-    'harvard.wav', which actually is 18.356190476190477.
+    "harvard.wav", which actually is 18.356190476190477.
 
     """
     precision = 1e-15
@@ -170,7 +161,7 @@ def test_duration_2():
 
 
 def test_left_channel_1():
-    """ Test the left_channel method of a mono file 'mono_22050_16bits.wav',
+    """ Test the left_channel method of a mono file "mono_22050_16bits.wav",
     where mono_22050_16bits.data is the only channel.
 
     """
@@ -179,7 +170,7 @@ def test_left_channel_1():
 
 
 def test_left_channel_2():
-    """ Test the left_channel method of a stereo file 'harvard.wav',
+    """ Test the left_channel method of a stereo file "harvard.wav",
     where harvard.data[:, 0]  is the left channel.
 
     """
@@ -188,7 +179,7 @@ def test_left_channel_2():
 
 
 def test_right_channel_1():
-    """ Test the right_channel method of a mono file 'mono_22050_16bits.wav',
+    """ Test the right_channel method of a mono file "mono_22050_16bits.wav",
     where mono_22050_16bits.data is the only channel.
 
     """
@@ -197,7 +188,7 @@ def test_right_channel_1():
 
 
 def test_right_channel_2():
-    """ Test the rigth_channel method of a stereo file 'harvard.wav',
+    """ Test the rigth_channel method of a stereo file "harvard.wav",
     where harvard.data[:, 1]  is the right channel.
 
     """
@@ -207,7 +198,7 @@ def test_right_channel_2():
 
 def test_max_1():
     """ Test the maximum PCM (amplitude) value of the file
-    'mono_22050_16bits.wav', which actually is 32767.
+    "mono_22050_16bits.wav", which actually is 32767.
 
     """
     assert mono_22050_16bits.max() == 32767
@@ -215,7 +206,7 @@ def test_max_1():
 
 def test_max_2():
     """ Test the maximum PCM (amplitude) value of the file
-    'harvard.wav', which actually is 20329.
+    "harvard.wav", which actually is 20329.
 
     """
     assert harvard.max() == 20329
@@ -223,7 +214,7 @@ def test_max_2():
 
 def test_min_1():
     """ Test the minimum PCM (amplitude) value of the file
-    'mono_22050_16bits.wav', which actually is -32768.
+    "mono_22050_16bits.wav", which actually is -32768.
 
     """
     assert mono_22050_16bits.min() == -32768
@@ -231,7 +222,7 @@ def test_min_1():
 
 def test_min_2():
     """ Test the minimum PCM (amplitude) value of the file
-    'harvard.wav', which actually is -32768.
+    "harvard.wav", which actually is -32768.
 
     """
     assert harvard.min() == -32768
